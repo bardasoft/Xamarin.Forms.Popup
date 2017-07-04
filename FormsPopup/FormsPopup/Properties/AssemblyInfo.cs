43fd16c1cfc21dfa5314cf6d676d65ca2bc8abee
lightwaver <lightwaver@gmx.at>
2017-07-04 07:37:52 +0200

    .nuspec File created

--- FormsPopup/FormsPopup/Properties/AssemblyInfo.cs
+++ FormsPopup/FormsPopup/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FormsPopup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MWX.XamForms.Popup")]
+[assembly: AssemblyDescription("Library for Xamarin.Forms Popup and TemplatedPicker")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FormsPopup")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("marekworks.at - Stefan M. Marek")]
+[assembly: AssemblyProduct("MWX.XamForms.Popup")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
